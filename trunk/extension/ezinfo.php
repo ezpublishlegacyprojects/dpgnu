dd297539df48e93546fd8d6e64f282f3ad109dbd
dpitard <info@example.com>
2009-05-27 10:14:31 +0000

    import 2.0.3 release
    git-svn-id: http://svn.projects.ez.no/dpgnu@2 6192a314-ab78-469e-a2e3-950e21a1cac2

---
+++ trunk/extension/ezinfo.php
@@ -0,0 +1,13 @@
+<?php
+
+class dpgnuInfo
+{
+    static function info()
+    {
+        return array( 'name' => 'dP Google News URL',
+                      'version' => '2.0.3',
+                      'copyright' => 'Copyright (C) 2007 Damien Pitard',
+                      'license'   => 'GNU General Public License v2.0',);
+    }
+}
+?>
---
+++ trunk/extension/settings/site.ini.append.php
@@ -0,0 +1,15 @@
+<?php /* #?ini charset="iso-8859-1"?
+
+[URLTranslator]
+
+# A list of extension which have url filter support.
+Extensions[]=dpgnu
+
+# List of active filters, each entry must contain the name of the class
+# which implements the filtering. The name of the file is a lowercase
+# variant of the classname.
+# e.g. the class MyFilter would have the filename myfilter.php
+# Note: All filters are placed in the subdirectory urlfilters in extension directory.
+Filters[]=dpgnu
+
+*/ ?>
